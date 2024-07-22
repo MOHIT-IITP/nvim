62efe0d02ae50a32e122c1b3751e6626b6175e9e
MOHIT-IITP <codingbymohit@gmail.com>
2024-07-22 20:16:56 +0530

    update and add new theme night fox

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,14 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme "catppuccin"
+--   end,
+-- }
+return { "EdenEast/nightfox.nvim",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme "carbonfox"
   end,
-}
+} 
