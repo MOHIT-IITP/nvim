ce05a836014521cf1918bb1b50f9b79afa701bd5
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-05-12 23:05:20 +0530

    Update: Major Lsp config takes 2hrs

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,117 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp"
+    },
+    config = function()
+      -- Configure capabilities
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      -- Configure servers
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'lua_ls',       -- Lua
+          'pyright',      -- Python
+          'rust_analyzer',-- Rust
+          'tsserver',     -- TypeScript/JavaScript
+          'clangd',       -- C/C++
+          'bashls',       -- Bash
+          'jsonls',       -- JSON
+          'yamlls'        -- YAML
+        },
+        handlers = {
+          -- Default handler
+          function(server_name)
+            require('lspconfig')[server_name].setup({
+              capabilities = capabilities
+            })
+          end,
+          
+          -- Lua-specific config
+          ['lua_ls'] = function()
+            require('lspconfig').lua_ls.setup({
+              settings = {
+                Lua = {
+                  diagnostics = { globals = { 'vim' } },
+                  workspace = { checkThirdParty = false },
+                  telemetry = { enable = false }
+                }
+              }
+            })
+          end,
+          
+          -- Python-specific config
+          ['pyright'] = function()
+            require('lspconfig').pyright.setup({
+              settings = {
+                python = {
+                  analysis = {
+                    typeCheckingMode = "basic",
+                    autoSearchPaths = true,
+                    useLibraryCodeForTypes = true
+                  }
+                }
+              }
+            })
+          end
+        }
+      })
+
+      -- LSP keymaps
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf, silent = true }
+
+          -- Navigation
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+
+          -- Code actions
+          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+
+          -- Diagnostics
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+        end
+      })
+
+      -- Diagnostic configuration
+      vim.diagnostic.config({
+        virtual_text = false,
+        float = { border = 'rounded' },
+        signs = true,
+        update_in_insert = false
+      })
+
+      -- Custom diagnostic signs
+      local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+    end
+  }
+}
+
